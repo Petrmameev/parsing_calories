185ad3a23362b79b514ff7558cf94ef7a00e474a
Petrmameev <petr_mameev@mail.ru>
2024-01-29 22:08:47 +0700

    second commit

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import random
+import csv
+import json
 
+import lxml
 import requests
 from bs4 import BeautifulSoup
-import lxml
-import json
-import csv
-
 
 headers = {
     "Accept": "*/*",
@@ -35,74 +33,74 @@ def get_page(url):
         json.dump(all_categories, file, indent=4, ensure_ascii=False)
 
 
-with open("all_categories.json", encoding="utf-8") as file:
-    all_categories = json.load(file)
-iteration_count = int(len(all_categories)) - 1
-count = 0
-print(f"Всего итераций: {iteration_count}")
-for category_name, category_href in all_categories.items():
+    with open("all_categories.json", encoding="utf-8") as file:
+        all_categories = json.load(file)
+    iteration_count = int(len(all_categories)) - 1
+    count = 0
+    print(f"Всего итераций: {iteration_count}")
+    for category_name, category_href in all_categories.items():
 
-    rep = [",", " ", "-", "`"]
-    for item in rep:
-        if item in category_name:
-            category_name = category_name.replace(item, "_")
-    req = requests.get(url=category_href, headers=headers)
-    src = req.text
+        rep = [",", " ", "-", "`"]
+        for item in rep:
+            if item in category_name:
+                category_name = category_name.replace(item, "_")
+        req = requests.get(url=category_href, headers=headers)
+        src = req.text
 
-    with open(f"data/{category_name}.html", "w", encoding="utf-8") as file:
-        file.write(src)
+        with open(f"data/{category_name}.html", "w", encoding="utf-8") as file:
+            file.write(src)
 
-    with open(f"data/{category_name}.html", "r", encoding="utf-8") as file:
-        src = file.read()
+        with open(f"data/{category_name}.html", "r", encoding="utf-8") as file:
+            src = file.read()
 
-    soup = BeautifulSoup(src, "lxml")
+        soup = BeautifulSoup(src, "lxml")
 
-    alert_block = soup.find(class_="uk-alert-danger")
-    if alert_block is not None:
-        continue
-    table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
-    product = table_head[0].text
-    calories = table_head[1].text
-    proteins = table_head[2].text
-    fats = table_head[3].text
-    carbohydrates = table_head[4].text
+        alert_block = soup.find(class_="uk-alert-danger")
+        if alert_block is not None:
+            continue
+        table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
+        product = table_head[0].text
+        calories = table_head[1].text
+        proteins = table_head[2].text
+        fats = table_head[3].text
+        carbohydrates = table_head[4].text
 
-    with open(f"data{count}_{category_name}.csv", "w", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        writer.writerow((product, calories, proteins, fats, carbohydrates))
+        with open(f"data/{count}_{category_name}.csv", "w", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow((product, calories, proteins, fats, carbohydrates))
 
-    products_data = soup.find(class_="mzr-tc-group-table").find("tbody").find_all("tr")
+        products_data = soup.find(class_="mzr-tc-group-table").find("tbody").find_all("tr")
 
-    products_info = []
-    for item in products_data:
-        product_tds = item.find_all("td")
-        title = product_tds[0].find("a").text
-        calories = product_tds[1].text
-        proteins = product_tds[2].text
-        fats = product_tds[3].text
-        carbohydrates = product_tds[4].text
+        products_info = []
+        for item in products_data:
+            product_tds = item.find_all("td")
+            title = product_tds[0].find("a").text
+            calories = product_tds[1].text
+            proteins = product_tds[2].text
+            fats = product_tds[3].text
+            carbohydrates = product_tds[4].text
 
-        products_info.append(
-            {
-                "Title": title,
-                "Calories": calories,
-                "Proteins": proteins,
-                "Fats": fats,
-                "Carbohydrates": carbohydrates,
-            }
-        )
-        with open(f"data{count}_{category_name}.csv", "a", encoding="utf-8") as file:
+            products_info.append(
+                {
+                    "Title": title,
+                    "Calories": calories,
+                    "Proteins": proteins,
+                    "Fats": fats,
+                    "Carbohydrates": carbohydrates,
+                }
+            )
+        with open(f"data/{count}_{category_name}.csv", "a", encoding="utf-8") as file:
             writer = csv.writer(file)
             writer.writerow((title, calories, proteins, fats, carbohydrates))
-    with open(f"data/{count}_{category_name}.json", "a", encoding="utf-8") as file:
-        json.dump(products_info, file, indent=4, ensure_ascii=False)
-    count = +1
-    print(f"Итерация{count}. {category_name} записан.....")
-    iteration_count = iteration_count - 1
-    if iteration_count == 0:
-        print("Парсинг завершен")
-        break
-    print(f"Осталось итераций: {iteration_count}")
+        with open(f"data/{count}_{category_name}.json", "a", encoding="utf-8") as file:
+            json.dump(products_info, file, indent=4, ensure_ascii=False)
+        count = +1
+        print(f"Итерация{count}. {category_name} записан.....")
+        iteration_count = iteration_count - 1
+        if iteration_count == 0:
+            print("Парсинг завершен")
+            break
+        print(f"Осталось итераций: {iteration_count}")
 
 
 def main():
